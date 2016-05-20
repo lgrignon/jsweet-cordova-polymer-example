43ef4c570454956c736199227b6b49e88e99f713
Louis Grignon <louis.grignon@gmail.com>
2016-05-20 16:59:47 +0200

    initial add task list tutorial

---
+++ README.md
@@ -0,0 +1,20 @@
+# Cordova Polymer TODO list app
+
+Based on [this tutorial](http://code.tutsplus.com/tutorials/how-to-create-a-to-do-list-app-with-polymer-and-cordova--cms-25434)
+
+## Pre-requisits
+
+* Ensure java version is 8 using `java -version`
+* Ensure maven command line (mvn) is in PATH by typing `mvn -version`, if command not found, download apache maven 3.3.x and include its bin folder in PATH, then try again
+* Ensure node packet manager (npm) is in PATH by typing `npm -v`, if command not found, download and install NodeJS, then try again
+* Install Cordova 5.3 if not done yet: `npm install cordova@5.3.3 -g`, -g switch means install globally
+
+## Compiling
+
+Compile JSweet using
+```
+mvn clean generate-sources
+```
+
+JavaScript files are generated in `www/js/app/`, check that they are up to date
+
---
+++ src/main/java/org/jsweet/examples/polymerjs/components/TasksListComponent.java
@@ -0,0 +1,110 @@
+package org.jsweet.examples.polymerjs.components;
+
+import static def.polymer_ts.polymer.Globals.createElement;
+import static jsweet.dom.Globals.console;
+import static jsweet.dom.Globals.localStorage;
+import static jsweet.util.Globals.function;
+import static jsweet.util.Globals.object;
+
+import java.util.function.Function;
+
+import def.polymer_ts.polymer.Base;
+import jsweet.dom.HTMLElement;
+import jsweet.lang.Interface;
+
+interface PaperDialog {
+	void open();
+
+	void close();
+}
+
+@Interface
+abstract class PaperItemModel<TItem> {
+	TItem item;
+}
+
+@Interface
+abstract class PaperItemEvent<TItem> {
+	PaperItemModel<TItem> model;
+}
+
+class Task {
+	public String taskName;
+	public boolean isComplete;
+}
+
+public class TasksListComponent extends Base {
+
+	public TasksListComponent() {
+		this.is = "tasks-list";
+
+		console.log("created " + is);
+	}
+
+	public static void main() {
+		createElement((Function<Object, TasksListComponent>) function(TasksListComponent.class));
+		console.log("registered task list component");
+	}
+
+	public String latestTaskName;
+	public Task[] tasks = {};
+
+	public void showAddTaskDialog() {
+		PaperDialog dialog = this.findInnerElement("addTaskDialog");
+		dialog.open();
+	}
+
+	public void addTask() {
+		// Store the new task as not completed
+		localStorage.setItem(this.latestTaskName, "no");
+
+		// Reset latestTaskName
+		this.latestTaskName = "";
+
+		// Close the dialog
+		PaperDialog dialog = this.findInnerElement("addTaskDialog");
+		dialog.close();
+
+		// Update the list of tasks
+		this.updateTasks();
+	}
+
+	public void toggleTask(PaperItemEvent<Task> event) {
+		// Get the name of the task
+		String taskName = event.model.item.taskName;
+
+		// Convert true/false to yes/no
+		if (event.model.item.isComplete)
+			localStorage.setItem(taskName, "yes");
+		else
+			localStorage.setItem(taskName, "no");
+	}
+
+	public void deleteTask(PaperItemEvent<Task> event) {
+		String taskName = event.model.item.taskName;
+		localStorage.removeItem(taskName);
+
+		// Update the list of tasks
+		this.updateTasks();
+	}
+
+	public void updateTasks() {
+		// Empty the array
+		this.splice("tasks", 0, tasks.length);
+
+		// Add items from localStorage
+		for (String savedTaskName : localStorage) {
+			Task task = new Task() {
+				{
+					taskName = savedTaskName;
+					isComplete = localStorage.getItem(savedTaskName) == "yes";
+				}
+			};
+			this.push("tasks", task);
+		}
+	}
+
+	protected <T> T findInnerElement(String id) {
+		return (T) object($).$get(id);
+	}
+}
